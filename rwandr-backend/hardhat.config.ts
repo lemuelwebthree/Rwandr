7c4236e4f078486dd59349aa4b154c3cf11ed6c8
Lemuel Solomon <alvinandwealth@gmail.com>
2024-10-18 21:44:53 +0200

    set hardhat config

--- rwandr-backend/hardhat.config.ts
+++ rwandr-backend/hardhat.config.ts
@@ -1,8 +1,18 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.27",
+  solidity: "0.8.20",
+  networks: {
+    sepolia: {
+      url: process.env.HTTP_PROVIDER_URL,
+      accounts: [process.env.PRIVATE_KEY as string],
+    },
+  },
+  gasReporter: { enabled: true },
 };
 
 export default config;
